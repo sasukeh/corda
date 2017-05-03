cf8515a0556e88aba67eb3aca5bd4fd1c00749b9
RogerWillis <roger.willis@r3cev.com>
2017-05-03 15:09:35 +0100

    Made changes to accommodate new CollectSignaturesFlow approach.

--- samples/irs-demo/src/main/kotlin/net/corda/irs/plugin/IRSPlugin.kt
+++ samples/irs-demo/src/main/kotlin/net/corda/irs/plugin/IRSPlugin.kt
@@ -3,12 +3,9 @@ package net.corda.irs.plugin
 import net.corda.core.contracts.StateRef
 import net.corda.core.crypto.Party
 import net.corda.core.node.CordaPluginRegistry
-import net.corda.core.transactions.SignedTransaction
-import net.corda.flows.CollectSignaturesFlow
 import net.corda.irs.api.InterestRateSwapAPI
 import net.corda.irs.contract.InterestRateSwap
 import net.corda.irs.flows.AutoOfferFlow
-import net.corda.irs.flows.CollectSignatureFlowImpl
 import net.corda.irs.flows.FixingFlow
 import net.corda.irs.flows.UpdateBusinessDayFlow
 import java.time.Duration
@@ -20,11 +17,10 @@ class IRSPlugin : CordaPluginRegistry() {
     override val staticServeDirs: Map<String, String> = mapOf(
             "irsdemo" to javaClass.classLoader.getResource("irsweb").toExternalForm()
     )
-    override val servicePlugins = listOf(Function(FixingFlow::Service), Function(CollectSignatureFlowImpl::Service))
+    override val servicePlugins = listOf(Function(FixingFlow::Service))
     override val requiredFlows: Map<String, Set<String>> = mapOf(
             AutoOfferFlow.Requester::class.java.name to setOf(InterestRateSwap.State::class.java.name),
             UpdateBusinessDayFlow.Broadcast::class.java.name to setOf(LocalDate::class.java.name),
             FixingFlow.FixingRoleDecider::class.java.name to setOf(StateRef::class.java.name, Duration::class.java.name),
-            FixingFlow.Floater::class.java.name to setOf(Party::class.java.name, FixingFlow.FixingSession::class.java.name),
-            CollectSignaturesFlow::class.java.name to setOf(SignedTransaction::class.java.name))
+            FixingFlow.Floater::class.java.name to setOf(Party::class.java.name, FixingFlow.FixingSession::class.java.name))
 }
