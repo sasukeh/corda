8eab3cabe9ebb8df4b11010eec2656e0cc3a888d
Clinton Alexander <clinton.alexander@r3cev.com>
2017-05-04 17:19:32 +0100

    Reverted a dev mode change.

--- core/src/main/kotlin/net/corda/core/node/services/TimestampChecker.kt
+++ core/src/main/kotlin/net/corda/core/node/services/TimestampChecker.kt
@@ -3,6 +3,7 @@ package net.corda.core.node.services
 import net.corda.core.contracts.Timestamp
 import net.corda.core.seconds
 import net.corda.core.until
+import net.corda.core.utilities.loggerFor
 import java.time.Clock
 import java.time.Duration
 
@@ -11,11 +12,17 @@ import java.time.Duration
  */
 class TimestampChecker(val clock: Clock = Clock.systemUTC(),
                        val tolerance: Duration = 30.seconds) {
+    private companion object {
+        val logger = loggerFor<TimestampChecker>()
+    }
+
     fun isValid(timestampCommand: Timestamp): Boolean {
         val before = timestampCommand.before
         val after = timestampCommand.after
 
         val now = clock.instant()
+
+        logger.info("Checking timestamp - before: $before, after: $after, now: $now")
 
         // We don't need to test for (before == null && after == null) or backwards bounds because the TimestampCommand
         // constructor already checks that.
